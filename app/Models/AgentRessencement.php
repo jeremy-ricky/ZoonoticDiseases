6acd7a289e747cea1cee5d601b598789fc43e898
VainqueurBul <vainqueurinfo1@gmail.com>
2020-11-05 16:46:44 +0100

    Modifications views

--- app/Models/AgentRessencement.php
+++ app/Models/AgentRessencement.php
@@ -7,4 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 class AgentRessencement extends Model
 {
     protected $fillable = ['Nom', 'Sexe', 'Telephone', 'Email', 'Etat'];
+
+    public static function UniqueEmail($Email){
+    	$Nbre = AgentRessencement::whereEmail($Email)->count('id');
+
+    	if ($Nbre==0) {
+    		return false;
+    	}else{
+    		return true;
+    	}
+    }
+
+    public static function UniqueTelephone($Telephone){
+    	$Nbre = AgentRessencement::whereTelephone($Telephone)->count('id');
+
+    	if ($Nbre==0) {
+    		return false;
+    	}else{
+    		return true;
+    	}
+    }
 }
