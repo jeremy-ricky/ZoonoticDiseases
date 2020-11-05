df472cbcff3dc2ce867436acc1fa0082e6d8a19b
VainqueurBul <vainqueurinfo1@gmail.com>
2020-11-05 16:37:25 +0100

    Modifications views

--- resources/views/Embarquements/_Form.blade.php
+++ resources/views/Embarquements/_Form.blade.php
@@ -1,350 +1,49 @@
    
           <div class="card-body">
             <div class="row">
+              
               <div class="col-md-6">
+               
                 <div class="form-group">
-                  <label>Minimal</label>
-                  <select class="form-control select2" style="width: 100%;">
-                    <option selected="selected">Alabama</option>
-                    <option>Alaska</option>
-                    <option>California</option>
-                    <option>Delaware</option>
-                    <option>Tennessee</option>
-                    <option>Texas</option>
-                    <option>Washington</option>
+                  <label>Campagne *</label>
+                  
+                  <select class="form-control select2" name="Campagne">
+                       <option value="1">Campagne</option>
                   </select>
-                </div>
-              </div>
-              <!-- /.col -->
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label>Multiple</label>
-                  <select class="select2" multiple="multiple" data-placeholder="Select a State" style="width: 100%;">
-                    <option>Alabama</option>
-                    <option>Alaska</option>
-                    <option>California</option>
-                    <option>Delaware</option>
-                    <option>Tennessee</option>
-                    <option>Texas</option>
-                    <option>Washington</option>
-                  </select>
-                </div>
-              </div>
-              <!-- /.col -->
-
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label>Intl US phone mask:</label>
-
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text"><i class="fas fa-phone"></i></span>
-                    </div>
-                    <input type="text" class="form-control"
-                           data-inputmask="'mask': ['999-999-9999 [x99999]', '+099 99 99 9999[9]-9999']" data-mask>
-                  </div>
-                  <!-- /.input group -->
-                </div>
-
-                <!-- Color Picker -->
-                <div class="form-group">
-                  <label>Color picker:</label>
-                  <input type="text" class="form-control my-colorpicker1">
+                  {!! $errors->first('Campagne', '<span class="error">:message</span>') !!}
                 </div>
                 <!-- /.form group -->
 
                 <!-- Color Picker -->
                 <div class="form-group">
-                  <label>Color picker with addon:</label>
-
-                  <div class="input-group my-colorpicker2">
-                    <input type="text" class="form-control">
-
-                    <div class="input-group-append">
-                      <span class="input-group-text"><i class="fas fa-square"></i></span>
-                    </div>
-                  </div>
-                  <!-- /.input group -->
+                  <label>Port *</label>
+                  <select class="form-control select2" name="Port">
+                       <option value="1">Port</option>
+                  </select>
+                  {!! $errors->first('Port', '<span class="error">:message</span>') !!}
                 </div>
                 <!-- /.form group -->
-
-                <!-- time Picker -->
-                <div class="bootstrap-timepicker">
-                  <div class="form-group">
-                    <label>Time picker:</label>
-
-                    <div class="input-group date" id="timepicker" data-target-input="nearest">
-                      <input type="text" class="form-control datetimepicker-input" data-target="#timepicker"/>
-                      <div class="input-group-append" data-target="#timepicker" data-toggle="datetimepicker">
-                          <div class="input-group-text"><i class="far fa-clock"></i></div>
-                      </div>
-                      </div>
-                    <!-- /.input group -->
-                  </div>
-                  <!-- /.form group -->
-                </div>
-              </div>
-
+               </div>
               <div class="col-md-6">
                 <div class="form-group">
-                  <label>Date:</label>
-                    <div class="input-group date" id="reservationdate" data-target-input="nearest">
-                        <input type="text" class="form-control datetimepicker-input" data-target="#reservationdate"/>
-                        <div class="input-group-append" data-target="#reservationdate" data-toggle="datetimepicker">
-                            <div class="input-group-text"><i class="fa fa-calendar"></i></div>
-                        </div>
-                    </div>
+                  <label>Date Emb. *</label>
+                  <input type="date" class="form-control" name="DateE" value="{{ old('DateE')}}">
+                  {!! $errors->first('DateE', '<span class="error">:message</span>') !!}
                 </div>
 
                 <div class="form-group">
-                  <label>Date:</label>
-                    <div class="input-group date" id="reservationdate" data-target-input="nearest">
-                        <input type="text" class="form-control datetimepicker-input" data-target="#reservationdate"/>
-                        <div class="input-group-append" data-target="#reservationdate" data-toggle="datetimepicker">
-                            <div class="input-group-text"><i class="fa fa-calendar"></i></div>
-                        </div>
-                    </div>
+                  <label>Heure Emb. *</label>
+                  <input type="time" class="form-control" name="HeureE" value="{{ old('HeureE') }}">
+                  {!! $errors->first('HeureE', '<span class="error">:message</span>') !!}
                 </div>
-                <!-- /.form group -->
-                <!-- Date range -->
-                <div class="form-group">
-                  <label>Date range:</label>
-
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text">
-                        <i class="far fa-calendar-alt"></i>
-                      </span>
-                    </div>
-                    <input type="text" class="form-control float-right" id="reservation">
-                  </div>
-                  <!-- /.input group -->
-                </div>
-                <!-- /.form group -->
-
-                <!-- Date and time range -->
-                <div class="form-group">
-                  <label>Date and time range:</label>
-
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text"><i class="far fa-clock"></i></span>
-                    </div>
-                    <input type="text" class="form-control float-right" id="reservationtime">
-                  </div>
-                  <!-- /.input group -->
-                </div>
-                <!-- /.form group -->
-
-                <!-- Date and time range -->
-                <div class="form-group">
-                  <label>Date range button:</label>
-
-                  <div class="input-group">
-                    <button type="button" class="btn btn-default float-right" id="daterange-btn">
-                      <i class="far fa-calendar-alt"></i> Date range picker
-                      <i class="fas fa-caret-down"></i>
-                    </button>
-                  </div>
-                </div>
-                <!-- /.form group -->
               </div>
+            </div>
 
-
-              <div class="col-sm-6">
-                    <!-- checkbox -->
-                    <div class="form-group clearfix">
-                      <div class="icheck-primary d-inline">
-                        <input type="checkbox" id="checkboxPrimary1" checked>
-                        <label for="checkboxPrimary1">
-                        </label>
-                      </div>
-                      <div class="icheck-primary d-inline">
-                        <input type="checkbox" id="checkboxPrimary2">
-                        <label for="checkboxPrimary2">
-                        </label>
-                      </div>
-                      <div class="icheck-primary d-inline">
-                        <input type="checkbox" id="checkboxPrimary3" disabled>
-                        <label for="checkboxPrimary3">
-                          Primary checkbox
-                        </label>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-sm-6">
-                    <!-- radio -->
-                    <div class="form-group clearfix">
-                      <div class="icheck-primary d-inline">
-                        <input type="radio" id="radioPrimary1" name="r1" checked>
-                        <label for="radioPrimary1">
-                        </label>
-                      </div>
-                      <div class="icheck-primary d-inline">
-                        <input type="radio" id="radioPrimary2" name="r1">
-                        <label for="radioPrimary2">
-                        </label>
-                      </div>
-                      <div class="icheck-primary d-inline">
-                        <input type="radio" id="radioPrimary3" name="r1" disabled>
-                        <label for="radioPrimary3">
-                          Primary radio
-                        </label>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <!-- Minimal red style -->
-                <div class="row">
-                  <div class="col-sm-6">
-                    <!-- checkbox -->
-                    <div class="form-group clearfix">
-                      <div class="icheck-danger d-inline">
-                        <input type="checkbox" checked id="checkboxDanger1">
-                        <label for="checkboxDanger1">
-                        </label>
-                      </div>
-                      <div class="icheck-danger d-inline">
-                        <input type="checkbox" id="checkboxDanger2">
-                        <label for="checkboxDanger2">
-                        </label>
-                      </div>
-                      <div class="icheck-danger d-inline">
-                        <input type="checkbox" disabled id="checkboxDanger3">
-                        <label for="checkboxDanger3">
-                          Danger checkbox
-                        </label>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-sm-6">
-                    <!-- radio -->
-                    <div class="form-group clearfix">
-                      <div class="icheck-danger d-inline">
-                        <input type="radio" name="r2" checked id="radioDanger1">
-                        <label for="radioDanger1">
-                        </label>
-                      </div>
-                      <div class="icheck-danger d-inline">
-                        <input type="radio" name="r2" id="radioDanger2">
-                        <label for="radioDanger2">
-                        </label>
-                      </div>
-                      <div class="icheck-danger d-inline">
-                        <input type="radio" name="r2" disabled id="radioDanger3">
-                        <label for="radioDanger3">
-                          Danger radio
-                        </label>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <!-- Minimal red style -->
-                <div class="row">
-                  <div class="col-sm-6">
-                    <!-- checkbox -->
-                    <div class="form-group clearfix">
-                      <div class="icheck-success d-inline">
-                        <input type="checkbox" checked id="checkboxSuccess1">
-                        <label for="checkboxSuccess1">
-                        </label>
-                      </div>
-                      <div class="icheck-success d-inline">
-                        <input type="checkbox" id="checkboxSuccess2">
-                        <label for="checkboxSuccess2">
-                        </label>
-                      </div>
-                      <div class="icheck-success d-inline">
-                        <input type="checkbox" disabled id="checkboxSuccess3">
-                        <label for="checkboxSuccess3">
-                          Success checkbox
-                        </label>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-sm-6">
-                    <!-- radio -->
-                    <div class="form-group clearfix">
-                      <div class="icheck-success d-inline">
-                        <input type="radio" name="r3" checked id="radioSuccess1">
-                        <label for="radioSuccess1">
-                        </label>
-                      </div>
-                      <div class="icheck-success d-inline">
-                        <input type="radio" name="r3" id="radioSuccess2">
-                        <label for="radioSuccess2">
-                        </label>
-                      </div>
-                      <div class="icheck-success d-inline">
-                        <input type="radio" name="r3" disabled id="radioSuccess3">
-                        <label for="radioSuccess3">
-                          Success radio
-                        </label>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="col-md-6">
-                    <div class="card-body">
-                      <input type="checkbox" name="my-checkbox" checked data-bootstrap-switch>
-                      <input type="checkbox" name="my-checkbox" checked data-bootstrap-switch data-off-color="danger" data-on-color="success">
-                    </div>
-                  </div>
-
-                  <div class="col-md-6">
-                      
-                <div class="card-body">
-                  <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="text" class="form-control" id="exampleInputEmail1" name="val1" value="1">
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="text" class="form-control" id="exampleInputPassword1" name="val2" value="1">
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleInputFile">File input</label>
-                    <div class="input-group">
-                      <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="exampleInputFile">
-                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                  </div>
-                </div>
-                <!-- /.card-body -->
-
-                <div class="card-footer">
+            <div class="card-footer">
                   <button type="submit" class="btn btn-success swalDefaultSuccess"><i class="fas fa-save"></i> Register</button>
                   <button type="submit" class="btn btn-danger swalDefaultError"><i class="fas fa-undo"></i> Cancel</button>
                   <button type="submit" class="btn btn-info"><i class="fas fa-edit"></i> Update</button>
                 </div>
-              
-                  </div>
-
-
-             
-              <div class="col-12">
-                <div class="form-group">
-                  <label>Multiple</label>
-                  <select class="duallistbox" multiple="multiple">
-                    <option selected>Alabama</option>
-                    <option>Alaska</option>
-                    <option>California</option>
-                    <option>Delaware</option>
-                    <option>Tennessee</option>
-                    <option>Texas</option>
-                    <option>Washington</option>
-                  </select>
-                </div>
-                <!-- /.form-group -->
-              </div>
-
-            </div>
           </div>
           <!-- /.card-body -->
           <div class="card-footer">
